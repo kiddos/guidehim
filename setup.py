56e59e263961cd65efe111258b6324920483b284
kiddos <kiddo831007@gmail.com>
2017-04-15 15:15:28 +0800

    pygame dependencies

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
   author='kiddos',
   author_email='kiddo831007@gmail.com',
   packages=['guidehim'],
-  install_requires=['numpy>=1.10.x', 'gym>=0.8.0']
+  install_requires=['numpy>=1.10.x', 'gym>=0.8.0', 'pygame>=1.9.3']
 )
