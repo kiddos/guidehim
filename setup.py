343ed59b45a365fed6338e1578feb1c3291a16cb
kiddos <kiddo831007@gmail.com>
2017-04-15 14:49:24 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+  name='guidehim',
+  version='0.1.0',
+  author='kiddos',
+  author_email='kiddo831007@gmail.com',
+  packages=['guidehim'],
+  install_requires=['numpy>=1.10.x', 'gym>=0.8.0']
+)
