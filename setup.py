8d61f6e87a08f7a68ad1219d6873e79b9bc4bf38
kiddos <kiddo831007@gmail.com>
2017-04-21 09:03:09 +0800

    install resource files

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os
 
 
 setup(
@@ -6,6 +7,7 @@ setup(
   version='0.1.0',
   author='kiddos',
   author_email='kiddo831007@gmail.com',
-  packages=['guidehim'],
+  packages=['guidehim', 'map'],
+  package_data={'guidehim': ['images/*.png'], 'map': ['*.map']},
   install_requires=['numpy>=1.10.x', 'gym>=0.8.0', 'pygame>=1.9.3']
 )
